a6d28096defb0fb807a618c385d618b1209eaf61
Atharva-Shelke <atharva.a.s.00@gmail.com>
2026-07-14 20:37:13 +0530

    Updated import from javax to jakarta for modern framework compatibility

--- src/main/java/com/Servlet1/Servlet1.java
+++ src/main/java/com/Servlet1/Servlet1.java
@@ -3,11 +3,11 @@ package com.Servlet1;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "Servlet1", urlPatterns = { "/Servlet1" })
 public class Servlet1 extends HttpServlet {
